b4c3fcec7a27f9f5f1e4491e617daabaddc020fa
SudKul <sudhanshu.kulshrestha@gmail.com>
2021-04-12 21:23:37 +0530

    Update Example 1

--- Examples_from_plants_database/Flask-CORS-Example-1/flaskr/__init__.py
+++ Examples_from_plants_database/Flask-CORS-Example-1/flaskr/__init__.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 from models import setup_db, Plant
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -15,15 +15,31 @@ def create_app(test_config=None):
         return response
     
 
-    @app.route('/plants')
+    @app.route('/plants', methods=['GET','POST'])
     #@cross_origin
     def get_plants():
-        # ToDo - Implement pagniation
+        # Implement pagniation
+        page = request.args.get('page', 1, type=int)
+        start = (page - 1) * 10
+        end = start + 10
+
         plants = Plant.query.all()
         formatted_plants = [plant.format() for plant in plants]
         return jsonify({
             'success': True,
-            'plants':formatted_plants
+            'plants':formatted_plants[start:end]
+            'total_plants':len(formatted_plants)
+            })
+
+    @app.route('/plants/<int:plant_id>')
+    def get_specific_plant(plant_id):
+        plant = Plant.query.filter(Plant.id==plant_id).one_or_none()
+        if plant is None:
+            abort(404)
+        else:   
+            return jsonify({
+                'success': True,
+                'plant': plant.format()
             })
 
     return app
