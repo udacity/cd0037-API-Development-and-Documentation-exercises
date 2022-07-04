f43f25c9859215749125c4190e6e3879662d09bf
Kachukwu Okoh <okohkachukwu@gmail.com>
2022-07-04 04:32:15 +0100

    Updated untracked files

--- 1_Requests_Starter/backend/models.py
+++ 1_Requests_Starter/backend/models.py
@@ -5,7 +5,7 @@ import json
 
 database_name = "bookshelf"
 database_path = "postgresql://{}:{}@{}/{}".format(
-    "student", "student", "localhost:5432", database_name
+    "postgres", "kosime1996", "localhost:5432", database_name
 )
 
 db = SQLAlchemy()
